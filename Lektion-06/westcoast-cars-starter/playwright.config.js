a74ffc129ab0eba63f56822d9a449cd29a2d4b02
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-05-10 18:17:45 +0200

    Added starter project for lesson 6

---
+++ Lektion-06/westcoast-cars-starter/playwright.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  reporter: 'html',
+
+  use: {
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
